f83b408a869c0b95100f95ad57731017e58cfeac
tyler tran <tyler.tran88@yahoo.com>
2026-07-03 20:50:53 -0700

    added af fixes

--- birdlib/camera.py
+++ birdlib/camera.py
@@ -1,17 +1,18 @@
 from picamera2 import Picamera2, Preview
+from libcamera import controls
 from config import TUNING_FILE
 
 def init_camera(width=1920, height=1080):
     tuning = Picamera2.load_tuning_file(TUNING_FILE) if TUNING_FILE else None
     picam2 = Picamera2(tuning=tuning)
 
-   
     picam2.configure(picam2.create_preview_configuration(
         main={"format": "RGB888", "size": (width, height)},
         #lores={"format": "YUV420", "size": (640, 480)}
     ))
     picam2.start_preview(Preview.DRM, x=0, y=0, width=width, height=height)
     picam2.start()
+    picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
     return picam2
 
 def capture_frame(picam2):
