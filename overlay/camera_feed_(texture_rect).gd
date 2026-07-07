fed7820eee4aa5735a04a1db3663b0236a1d285b
tyler tran <tyler.tran88@yahoo.com>
2026-07-06 21:33:35 -0700

    fix

--- overlay/camera_feed_(texture_rect).gd
+++ overlay/camera_feed_(texture_rect).gd
@@ -2,18 +2,27 @@ extends TextureRect
 
 var timer := 0.0
 var path = "/tmp/latest_frame.jpg"
+var cached_texture: ImageTexture = null
 
 func _process(delta):
 	timer += delta
-
 	if timer < 0.1:
 		return
-
 	timer = 0.0
 
-	if not FileAccess.file_exists(path):
+	var file = FileAccess.open(path, FileAccess.READ)
+	if file == null:
 		return
 
-	var image = Image.load_from_file(path)
-	if image != null:
-		texture = ImageTexture.create_from_image(image)
+	var bytes = file.get_buffer(file.get_length())
+	file.close()
+
+	var image = Image.new()
+	if image.load_jpg_from_buffer(bytes) != OK:
+		return
+
+	if cached_texture == null:
+		cached_texture = ImageTexture.create_from_image(image)
+		texture = cached_texture
+	else:
+		cached_texture.update(image)
